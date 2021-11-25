07bd8d89f905b57f2009f1695acc349be445ec16
masterSal <salimabdellanuru@gmail.com>
2021-11-25 13:29:04 +0300

    Q.12 fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False: return 0
-    letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string is None or isinstance(roman_string, str) is False: 
+        return 0
+    roman_letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     maxchar = 'I'
     total = 0
     for i in roman_string[::-1]:
-        if letters[i] >= letters[maxchar]:
+        if roman_letters[i] >= roman_letters[maxchar]:
             maxchar = i
-            total += letters[i]
+            total += roman_letters[i]
         else:
-            total -= letters[i]
+            total -= roman_letters[i]
     return total
