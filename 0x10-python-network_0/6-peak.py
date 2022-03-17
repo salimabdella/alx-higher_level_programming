f5dbb43d3dc09cc1160fd37eabc1e68c9b1658cb
masterSal <salimabdellanuru@gmail.com>
2022-03-17 18:38:08 +0300

    Q.6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Peak Module
+"""
+
+
+def divide(array, low, high):
+    """divide array, with high and low"""
+
+    mid = int((high + low)/2)
+    if array[mid-1] <= array[mid] >= array[mid+1]:
+        return array[mid]
+    elif array[mid] > array[mid+1]:
+        return divide(array, low, mid-1)
+    elif array[mid] < array[mid+1]:
+        return divide(array, mid+1, high)
+
+
+def find_peak(list_of_integers):
+    """Find peak of a list"""
+
+    if not list_of_integers:
+        return None
+    return divide(list_of_integers, 0, len(list_of_integers)-1)
